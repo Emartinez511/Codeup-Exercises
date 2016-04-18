9dbbbba0b560bbcd2db7621d405b255d3ced19ad
Emartinez511 <mrblonde9993@gmail.com>
2016-04-18 14:27:10 -0500

    I changed the intval to trim and also if letters are entered it will get a error.

--- for.php
+++ for.php
@@ -1,10 +1,24 @@
 <?php
 
 fwrite(STDOUT, "Choose a starting number!\n");
-$min = intval(fgets(STDIN));
+$min = trim(fgets(STDIN));
+
 fwrite(STDOUT, "Choose a ending number!\n");
-$max = intval(fgets(STDIN));
-$incr = intval(fgets(STDIN));
-for ($a = $min; $a <= $max; $a+=$incr) {
-    echo "$a\n";
+$max = trim(fgets(STDIN));
+
+fwrite(STDOUT, "Choose an increment to count by!\n");
+$incr = trim(fgets(STDIN));
+
+if ($min * $max != 0) {
+    if ($incr != Null){
+        for ($a = $min; $a <= $max; $a+=$incr) {
+            echo "$a\n";
+        }  
+    } else {
+        for ($a = $min; $a <= $max; $a++) {
+            echo "$a\n";
+        }
+    } 
+} else {
+    die("error: Enter only numbers\n");
 }
