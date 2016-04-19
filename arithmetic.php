c13c8def01aece43297da09683834ecf1d90cb51
Emartinez511 <mrblonde9993@gmail.com>
2016-04-19 14:27:03 -0500

    I used functions to do simple math.

---
+++ arithmetic.php
@@ -0,0 +1,27 @@
+<?php
+
+function add($a, $b)
+{
+    return $a + $b;
+}
+
+function subtract($a, $b)
+{
+    return $a - $b;
+}
+
+function multiply($a, $b)
+{
+    return $a * $b;
+}
+
+function divide($a, $b)
+{
+   return $a / $b;
+}
+
+// Add code to test your functions here
+echo add(12, 5) . PHP_EOL;
+echo subtract(12, 5) . PHP_EOL;
+echo multiply(12, 5) . PHP_EOL;
+echo divide(12, 6) . PHP_EOL;
