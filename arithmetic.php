820fc49580744daa9d785b53c5f3667e56060576
Emartinez511 <mrblonde9993@gmail.com>
2016-04-19 14:33:36 -0500

    I added a modulus to the project.

--- arithmetic.php
+++ arithmetic.php
@@ -1,5 +1,6 @@
 <?php
-
+$a = 15;
+$b = 2;
 function add($a, $b)
 {
     return $a + $b;
@@ -20,8 +21,15 @@ function divide($a, $b)
    return $a / $b;
 }
 
+function modulus($a, $b)
+{
+    return $a % $b;
+
+}
+
 // Add code to test your functions here
 echo add(12, 5) . PHP_EOL;
 echo subtract(12, 5) . PHP_EOL;
 echo multiply(12, 5) . PHP_EOL;
 echo divide(12, 6) . PHP_EOL;
+echo modulus(12, 4) . PHP_EOL;
